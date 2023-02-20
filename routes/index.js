e5d53e60dd45f76f6e360e964999a213df4371f3
giorgi kavtaradze <giorgikavtaradze@giorgis-MacBook-Air.local>
2023-02-20 18:01:44 +0400

    updating

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/hospital', function(req, res, next) {
   res.render('hospital', { title: 'Express' });
 });
 
